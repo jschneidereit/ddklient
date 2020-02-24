6d26d32c2f9be597a5f4c3aeddcff7362ddd7c95
Jim Schneidereit <jimschneidereit@gmail.com>
2020-02-23 16:29:10 -0800

    namecheap requires query string params

--- src/main/kotlin/ddklient/Clients.kt
+++ src/main/kotlin/ddklient/Clients.kt
@@ -4,9 +4,10 @@ import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
-import io.ktor.client.request.post
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.formUrlEncode
+import io.ktor.http.parametersOf
 import kotlinx.coroutines.runBlocking
 
 private val gson = Gson()
@@ -24,9 +25,17 @@ fun getIp() = runBlocking {
 fun submit(cache: Cache, config: Configuration): List<HttpResponse> = runBlocking {
     HttpClient(CIO).use { c ->
         getTasks(cache, config).map { task ->
-            c.post<HttpResponse> {
-                url(config.api())
-                body = gson.toJson(task)
+            when (task) {
+                is Namecheap -> { // TODO map tasks to requests instead of firing them in the switch
+                    val params = parametersOf(
+                            "host" to listOf(task.host),
+                            "ip" to listOf(task.ip),
+                            "domain" to listOf(task.domain),
+                            "password" to listOf(task.password)
+                    ).formUrlEncode()
+
+                    c.get<HttpResponse> { url("${config.api()}?$params") }
+                }
             }
         }
     }
